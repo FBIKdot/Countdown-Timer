7fe39a43895baa077a7ee7626c6cf5848634e897
FBIK <83176414+BovineBeta@users.noreply.github.com>
2022-03-14 16:57:06 +0800

    Update countdown-timer.js

--- countdown-timer.js
+++ countdown-timer.js
@@ -1,19 +1,17 @@
 function countTime() {
+
     //获取系统时间
-    //get system's time  
     var date = new Date();
     var now = date.getTime();
-    //设置截止时间  
-    //set time of death
-    var str = "2222/2/2 00:00:00";
+
+    //设置截止时间
+    //var str = "2222/2/2 00:00:00";
     var endDate = new Date(str);
     var end = endDate.getTime();
-
+    
     //时间差  
-    //time difference
     var leftTime = end - now;
-    //定义变量 d,h,m,s保存倒计时的时间  
-    //define variables d,h,m,s to save the countdown time
+    //定义变量 d,h,m,s保存倒计时的时间
     var d, h, m, s;
     if (leftTime >= 0) {
         d = Math.floor(leftTime / 1000 / 60 / 60 / 24);
@@ -21,14 +19,11 @@ function countTime() {
         m = Math.floor(leftTime / 1000 / 60 % 60);
         s = Math.floor(leftTime / 1000 % 60);
     }
-    //将倒计时赋值到span中
-    //set countdown to the "<span>"
+    //修改dom
     document.getElementById("_d").innerHTML = d;
     document.getElementById("_h").innerHTML = h;
     document.getElementById("_m").innerHTML = m;
     document.getElementById("_s").innerHTML = s;
-    //递归每秒调用countTime方法，显示动态时间效果
-    //Call the countTime method recursively every second to display the dynamic time effect  
-    setTimeout(countTime, 1000);
-
+    //递归，每秒调用countTime，显示动态时间效果
+    setTimeout("countTime()", 1000);
 }
