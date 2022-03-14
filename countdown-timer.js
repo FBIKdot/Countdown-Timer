6c7edc142391ea8405376d704291c2a041071737
FBIK <83176414+BovineBeta@users.noreply.github.com>
2022-03-14 17:02:27 +0800

    不分开修改dom

--- countdown-timer.js
+++ countdown-timer.js
@@ -1,4 +1,4 @@
-function countTime() {
+function cTimer() {
 
     //获取系统时间
     var date = new Date();
@@ -6,15 +6,16 @@ function countTime() {
 
     //设置截止时间
     //var str = "2222/2/2 00:00:00";
-    
+
     //获得截止时间
     var endDate = new Date(str);
     var end = endDate.getTime();
-    
+
     //时间差  
     var leftTime = end - now;
-    //定义变量 d,h,m,s保存倒计时的时间
+    //定义变量 d,h,m,s
     var d, h, m, s;
+    //赋值
     if (leftTime >= 0) {
         d = Math.floor(leftTime / 1000 / 60 / 60 / 24);
         h = Math.floor(leftTime / 1000 / 60 / 60 % 24);
@@ -22,10 +23,7 @@ function countTime() {
         s = Math.floor(leftTime / 1000 % 60);
     }
     //修改dom
-    document.getElementById("_d").innerHTML = d;
-    document.getElementById("_h").innerHTML = h;
-    document.getElementById("_m").innerHTML = m;
-    document.getElementById("_s").innerHTML = s;
-    //递归，每秒调用countTime，显示动态时间效果
-    setTimeout("countTime()", 1000);
+    document.getElementById("cTimer").innerHTML = d + "day&nbsp;" + h + "h&nbsp;" + m + "min&nbsp" + s + "s";
+    //递归. 每秒调用countTime来显示动态时间效果
+    setTimeout("cTimer()", 1000);
 }
