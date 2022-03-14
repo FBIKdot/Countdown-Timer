ea6d18f86bdb0c6aa338e62dd71699f02d37c792
FBIK <83176414+BovineBeta@users.noreply.github.com>
2022-03-14 17:07:06 +0800

    Update countdown-timer.easy.js

--- countdown-timer.easy.js
+++ countdown-timer.easy.js
@@ -1,7 +1,4 @@
-function countTime(){var date=new Date();var now=date.getTime();
+//设置截止时间
+//var str = "2222/2/2 00:00:00"
 
-//在这里设置倒计时日期与时间
-//set countdown date and time here
-var str="2222/2/2 00:00:00";
-
-var endDate=new Date(str);var end=endDate.getTime();var leftTime=end-now;var d,h,m,s;if(leftTime>=0){d=Math.floor(leftTime/1000/60/60/24);h=Math.floor(leftTime/1000/60/60%24);m=Math.floor(leftTime/1000/60%60);s=Math.floor(leftTime/1000%60)}document.getElementById("_d").innerHTML=d;document.getElementById("_h").innerHTML=h;document.getElementById("_m").innerHTML=m;document.getElementById("_s").innerHTML=s;setTimeout(countTime,1000)}
+function cTimer(){var date=new Date();var now=date.getTime();var endDate=new Date(str);var end=endDate.getTime();var leftTime=end-now;var d,h,m,s;if(leftTime>=0){d=Math.floor(leftTime/1000/60/60/24);h=Math.floor(leftTime/1000/60/60%24);m=Math.floor(leftTime/1000/60%60);s=Math.floor(leftTime/1000%60)}document.getElementById("cTimer").innerHTML=d+"day&nbsp;"+h+"h&nbsp;"+m+"min&nbsp"+s+"s";setTimeout("cTimer()",1000)}
