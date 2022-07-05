d9921131e78eaf503faf2c37d5e9459a013626ce
FBIK <83176414+BovineBeta@users.noreply.github.com>
2022-07-05 19:30:36 +0800

    更改了目标时间的设置方式

--- countdown-timer.easy.js
+++ countdown-timer.easy.js
@@ -1,4 +1,1 @@
-//设置截止时间
-//var str = "2222/2/2 00:00:00"
-
-function cTimer(){var date=new Date();var now=date.getTime();var endDate=new Date(str);var end=endDate.getTime();var leftTime=end-now;var d,h,m,s;if(leftTime>=0){d=Math.floor(leftTime/1000/60/60/24);h=Math.floor(leftTime/1000/60/60%24);m=Math.floor(leftTime/1000/60%60);s=Math.floor(leftTime/1000%60)};document.getElementById("cTimer").innerHTML=d+"day&nbsp;"+h+"h&nbsp;"+m+"min&nbsp"+s+"s";setTimeout("cTimer()",1000)};
+var str;function cTimer(sy,sm,sd){var date=new Date();var now=date.getTime();if(str==undefined){str=sy+"/"+sm+"/"+sd;console.log(str);if(sd==undefined){str=sy+"/"+sm+"/1"};if(sm==undefined){str=sy+"/1/1"};if(sy==undefined){str="2222/2/2"};console.log(str)};var endDate=new Date(str);var end=endDate.getTime();var leftTime=end-now;var d,h,m,s;if(leftTime>=0){d=Math.floor(leftTime/1000/60/60/24);h=Math.floor(leftTime/1000/60/60%24);m=Math.floor(leftTime/1000/60%60);s=Math.floor(leftTime/1000%60)};document.getElementById("cTimer").innerHTML=d+"day&nbsp;"+h+"h&nbsp;"+m+"min&nbsp"+s+"s";setTimeout("cTimer()",1000)};
--- countdown-timer.js
+++ countdown-timer.js
@@ -1,29 +1,46 @@
-function cTimer() {
+var str;
+function cTimer(sy, sm, sd) {
 
-    //获取系统时间
-    var date = new Date();
-    var now = date.getTime();
+	// 获取系统时间
+	var date = new Date();
+	var now = date.getTime();
 
-    //设置截止时间
-    //var str = "2222/2/2 00:00:00";
+	// 判断str是否被自定义
+	if (str == undefined) {
+		str = sy + "/" + sm + "/" + sd;
+		console.log(str);
+		if (sd == undefined) {
+			str = sy + "/" + sm + "/1";
+		}
+		;if (sm == undefined) {
+			str = sy + "/1/1";
+		}
+		;if (sy == undefined) {
+			str = "2222/2/2";
+		}
+		;console.log(str);
+	}
+	;// 获得截止时间
+	var endDate = new Date(str);
+	var end = endDate.getTime();
 
-    //获得截止时间
-    var endDate = new Date(str);
-    var end = endDate.getTime();
+	// 时间差
+	var leftTime = end - now;
 
-    //时间差  
-    var leftTime = end - now;
-    //定义变量 d,h,m,s
-    var d, h, m, s;
-    //赋值
-    if (leftTime >= 0) {
-        d = Math.floor(leftTime / 1000 / 60 / 60 / 24);
-        h = Math.floor(leftTime / 1000 / 60 / 60 % 24);
-        m = Math.floor(leftTime / 1000 / 60 % 60);
-        s = Math.floor(leftTime / 1000 % 60);
-    };
-    //修改dom
-    document.getElementById("cTimer").innerHTML = d + "day&nbsp;" + h + "h&nbsp;" + m + "min&nbsp" + s + "s";
-    //递归. 每秒调用countTime来显示动态时间效果
-    setTimeout("cTimer()", 1000);
-};
+	// 定义变量 d,h,m,s
+	var d, h, m, s;
+
+	// 赋值
+	if (leftTime >= 0) {
+		d = Math.floor(leftTime / 1000 / 60 / 60 / 24);
+		h = Math.floor(leftTime / 1000 / 60 / 60 % 24);
+		m = Math.floor(leftTime / 1000 / 60 % 60);
+		s = Math.floor(leftTime / 1000 % 60);
+	}
+	;// 修改dom
+	document.getElementById("cTimer").innerHTML = d + "day&nbsp;" + h + "h&nbsp;" + m + "min&nbsp" + s + "s";
+
+	// 递归. 每秒调用countTime来显示动态时间效果
+	setTimeout("cTimer()", 1000);
+}
+;
