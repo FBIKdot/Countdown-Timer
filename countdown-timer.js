f02e7437286409277ba4e32178bddb5e2a54ff92
FBIK <83176414+BovineBeta@users.noreply.github.com>
2022-07-06 11:51:02 +0800

    添加了函数参数为字符串时的目标时间设置功能

--- countdown-timer.js
+++ countdown-timer.js
@@ -1,49 +1,55 @@
 var str;
 function cTimer(sy, sm, sd) {
 
-	// 获取系统时间
-	var nowDate = new Date();
-	var now = nowDate.getTime();
+    // 获取系统时间
+    var nowDate = new Date();
+    var now = nowDate.getTime();
+    // 如果目标时间未定义
+    if (str == undefined) {
+        // 设置目标时间
+        str = sy + "/" + sm + "/" + sd;
+        // 如果参数是字符串
+        if (typeof sy == "string") {
+            str = sy;
+        } else {
+            // 如果只设置年月
+            if (sd == undefined) {
+                str = sy + "/" + sm + "/1";
+            }
+            ;//如果只设置年
+            if (sm == undefined) {
+                str = sy + "/1/1";
+            }
+            ;// 如果啥也没设置
+            if (sy == undefined) {
+                str = "2222/2/2";
+            }
+            ;
+        }
+        ;
+    }
+    ;// 获取截至时间
+    var endDate = new Date(str);
+    var end = endDate.getTime();
 
-	// 如果目标时间未定义
-	if (str == undefined) {
-		// 设置目标时间
-		str = sy + "/" + sm + "/" + sd;
-		// 如果只设置年月
-		if (sd == undefined) {
-			str = sy + "/" + sm + "/1";
-		}
-		;//如果只设置年
-		if (sm == undefined) {
-			str = sy + "/1/1";
-		}
-		;// 如果啥也没设置
-		if (sy == undefined) {
-			str = "2222/2/2";
-		}
-		;
-	}
-	;// 获取截至时间
-	var endDate = new Date(str);
-	var end = endDate.getTime();
+    // 得到剩余时间
+    var leftTime = end - now;
 
-	// 得到剩余时间
-	var leftTime = end - now;
+    // 定义变量 d,h,m,s
+    var d, h, m, s;
 
-	// 定义变量 d,h,m,s
-	var d, h, m, s;
-
-	// 将总时间拆开
-	if (leftTime >= 0) {
-		d = Math.floor(leftTime / 1000 / 60 / 60 / 24);
-		h = Math.floor(leftTime / 1000 / 60 / 60 % 24);
-		m = Math.floor(leftTime / 1000 / 60 % 60);
-		s = Math.floor(leftTime / 1000 % 60);
-	}
-	;// 修改dom
-	document.getElementById("cTimer").innerHTML = d + "day&nbsp;" + h + "h&nbsp;" + m + "min&nbsp" + s + "s";
-
-	// 递归. 每秒调用cTimer来显示动态时间效果
-	setTimeout("cTimer()", 1000);
+    // 将总时间拆开
+    if (leftTime >= 0) {
+        d = Math.floor(leftTime / 1000 / 60 / 60 / 24);
+        h = Math.floor(leftTime / 1000 / 60 / 60 % 24);
+        m = Math.floor(leftTime / 1000 / 60 % 60);
+        s = Math.floor(leftTime / 1000 % 60);
+        // 修改dom
+        document.getElementById("cTimer").innerHTML = d + "day&nbsp;" + h + "h&nbsp;" + m + "min&nbsp" + s + "s";
+    } else {
+        document.getElementById("cTimer").innerHTML = "The Countdown timer has stopped";
+    }
+    ;// 每秒重复执行cTimer, 使倒计时动态
+    setTimeout("cTimer();", 1000);
 }
 ;
