5b6881a0d0083a97d6d4b9603d853aa21b0ff6d4
FBIK <bovinefbik@foxmail.com>
2022-09-07 16:37:12 +0800

    删除无用代码

--- countdown-timer.js
+++ countdown-timer.js
@@ -4,6 +4,7 @@ function cTimer(sy, sm, sd) {
     // 获取系统时间
     var nowDate = new Date();
     var now = nowDate.getTime();
+
     // 如果目标时间未定义
     if (str == undefined) {
         // 设置目标时间
@@ -11,24 +12,15 @@ function cTimer(sy, sm, sd) {
         // 如果参数是字符串
         if (typeof sy == "string") {
             str = sy;
-        } else {
-            // 如果只设置年月
-            if (sd == undefined) {
-                str = sy + "/" + sm + "/1";
-            }
-            ;//如果只设置年
-            if (sm == undefined) {
-                str = sy + "/1/1";
-            }
-            ;// 如果啥也没设置
-            if (sy == undefined) {
-                str = "2222/2/2";
-            }
-            ;
-        }
-        ;
-    }
-    ;// 获取截至时间
+        } else if (sy == undefined) {
+            // 如果啥也没设置
+            str = "2222/2/2";
+
+        };
+        console.log(str);
+    };
+
+    // 获取截至时间
     var endDate = new Date(str);
     var end = endDate.getTime();
 
@@ -48,8 +40,8 @@ function cTimer(sy, sm, sd) {
         document.getElementById("cTimer").innerHTML = d + "day&nbsp;" + h + "h&nbsp;" + m + "min&nbsp" + s + "s";
     } else {
         document.getElementById("cTimer").innerHTML = "The Countdown timer has stopped";
-    }
-    ;// 每秒重复执行cTimer, 使倒计时动态
-    setTimeout("cTimer();", 1000);
-}
-;
+    };
+
+    // 每秒重复执行cTimer, 使倒计时动态
+    setTimeout(cTimer, 1000);
+};
